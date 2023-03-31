82bf531ef6a02d4245d2e1b4e1473adeb6103f06
Majid-pkz <majid.computing@gmail.com>
2023-03-31 23:33:43 +1100

    fix Reaction- it is schema not model

--- models/Reaction.js
+++ models/Reaction.js
@@ -32,6 +32,6 @@ const reactionSchema = new Schema(
     }
   );
   
-  const Reaction = model('reaction', reactionSchema);
+//   const Reaction = model('reaction', reactionSchema);
   
-  module.exports = Reaction;
+  module.exports = reactionSchema;
