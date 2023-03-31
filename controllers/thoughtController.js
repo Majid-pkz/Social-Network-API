64b3b9b22ab00473f5e7f1a0bc090262caecc682
Majid-pkz <majid.computing@gmail.com>
2023-03-31 23:36:14 +1100

    add thoughtController

---
+++ controllers/thoughtController.js
@@ -0,0 +1,58 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+
+     // Get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughtsData) => res.json(thoughtsData))
+      .catch((err) => console.log(err))
+    //   res.status(500).json(err));
+  },
+
+//   GET to get a single thought by its _id
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.toughtId })
+        // .select('-__v')       
+        .then((thoughtsData) =>
+            !thoughtsData
+            ? res.status(404).json({ message: 'No thought with that ID' })
+            : res.json(thoughtsData)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+
+// POST to create a new thought
+    createThought(req, res) {
+        Thought.create(req.body)
+        .then((thoughtsData) => {
+            return User.findOneAndUpdate(
+                {_id: req.body.userId},
+                {$push: {thoughts:thoughtsData._id}},
+                {new: true}
+                
+            );
+        })
+        
+        .then((thoughtsData) =>
+        !thoughtsData
+        ? res.status(404).json({ message: 'thought created but no user found with this Id' })
+        : res.json(thoughtsData)
+    )
+    .catch((err) => res.status(500).json(err));
+  },
+
+//PUT to update a thought by its _id
+    updateThought(req, res) {
+        Thought.findOneAndUpdate({ _id: req.params.toughtId }, {$set:req.bod})
+        .then((thoughtsData) =>       
+        !thoughtsData
+        ? res.status(404).json({ message: 'no thought with this Id ' })
+        : res.json(thoughtsData)
+    )
+    .catch((err) => res.status(500).json(err));
+    }
+
+
+
+};
